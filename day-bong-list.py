518e4c96585123d63d3d9c27998319d0334ba51c
multizone-quant <65776238+multizone-quant@users.noreply.github.com>
2020-05-23 00:26:55 +0900

    Create day-bong-list.py

---
+++ day-bong-list.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+# 현재 상장된 모든 종목의 일봉 정보를 json 파일로 저장
+# naver 아래 사이트의 정보를 크롤링하여 저장
+# https://finance.naver.com/sise/sise_market_sum.nhn
+# 단 아래 항목을 선택하여야 정상 동작함
+#   거래량, 시가, 고가, 저가, per, poe
+# 필요한 경우에 위 항목을 변경할 수 있음. 코드 상에서 title에서 변경한 항목 반영 필요함
+#
+# 종목이 많아서 page로 구성되어 있음. 마지막에 page 번호를 증가시키면서 계속 검색함
+# https://finance.naver.com/sise/sise_market_sum.nhn?&page=1
+# https://finance.naver.com/sise/sise_market_sum.nhn?&page=2
+#
+# 실행결과
+# 코스닥 상승 종목 : 날짜_kosdaq_day_bong_list.txt
+# 코스피 상승 종목 : 날짜_kospi_day_bong_list.txt
+#
+
+import time
+import urllib.request
+import json
+from bs4 import BeautifulSoup
+
+
+def load_json_from_file(file_name) :
+    try :
+        with open(file_name,'r',encoding="cp949") as make_file: 
+           data=json.load(make_file) 
+        make_file.close()
+    except  Exception as e : # 또는 except : 
+        data = {}
+        print(e, file_name)
+    return data
+
+def save_to_file_json(file_name, data) :
+    with open(file_name,'w',encoding="cp949") as make_file: 
+       json.dump(data, make_file, ensure_ascii=False, indent="\t") 
+    make_file.close()
+
+TODAY = time.strftime("%Y%m%d")
+
+# 특정 url에 있는 정보를 뽑아냄 
+def get_stock_list(url, cnt) :
+    title_list = ['no', 'name', 'close', 'diff', 'per', 'base_p', 'qty', 'open', 'high', 'low', 'per', 'pbr', 'talk']
+    with urllib.request.urlopen(url) as fs :
+        soup = BeautifulSoup(fs.read().decode(fs.headers.get_content_charset()), 'html.parser')
+
+    prices =[]
+    # 각 데이터는 tr로 시작
+    for tr in soup.find_all('tr') :
+        # 각 항목은 td로 시작
+        td_list = tr.find_all('td')
+        try : 
+            no = td_list[0].text.strip()
+            if no[0] == '\n' :
+                no = no.replace('\n','')
+            # 빈줄, 라인 등 데이터가 아닌 경우도 있다.
+            # 다행히 n 값에 1부터 증가하는 값이 기록되어 있으므로, 이 값이 맞으면 정상적인 데이터로 판단
+            if int(no) == cnt :
+                info = {}
+                for i in range(0,len(td_list)) :
+                    data = td_list[i].text.strip()
+                    info[title_list[i]] = data
+                if info['name'] == '2' : # 아래에 있는 페이지 정보이므로 무시
+                    continue
+                prices.append(info)
+                cnt+=1
+        except :
+            continue
+    return prices, cnt
+
+def day_bong_list(name) :
+    # 코스피
+    url1 = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0'
+    # 코스닥
+    url2 = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=1'
+    close_price_list = {'kospi':url1, 'kosdaq':url2}
+
+    # 총 페이지수, 상장 종목이 늘어나면 증가할 수 있음
+    close_page_list = {'kospi':32, 'kosdaq':29}
+
+    cnt = 1
+    prices = []
+    for i in range(0, close_page_list[name]) :
+        print('page ', i+1)
+        url = close_price_list[name] + '&page='+str(i+1)
+        ret, cnt = get_stock_list(url, cnt) 
+        prices += ret
+
+    # 저장
+    fname = TODAY+'_'+name+'_day_bong_list.txt'
+    save_to_file_json(fname, prices)
+    print('done ', name)
+
+print('kosdaq 전 종목 오늘 봉 저장 시작')
+day_bong_list('kosdaq')
+
+print('kospi 전 종목 오늘 봉 저장 시작')
+day_bong_list('kospi')
+
+
+
+# 잘 저장되어 있는지 test
+if 0 :
+    fname = TODAY+'_kosdaq'+'_day_bong_list.txt'
+    prices = load_json_from_file(fname)
+    for p in prices :
+        print(p)
